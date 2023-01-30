a11251868c5158954f1998697cb8f305a1e4ba49
Kyle Hall <kyle@bywatersolutions.com>
2023-01-30 10:52:48 -0500

    Update for Koha 22.11 on buster

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get update
-pbuilder create --distribution stretch --mirror http://debian.csail.mit.edu/debian/
+pbuilder create --distribution buster --mirror http://debian.csail.mit.edu/debian/
 pbuilder execute --save-after-exec /pbuilder.sh --basetgz /var/cache/pbuilder/base.tgz
 cp /var/cache/pbuilder/base.tgz /output/.
--- pbuilder.sh
+++ pbuilder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VERSION=21.05
+export VERSION=22.11
 echo "deb http://debian.koha-community.org/koha $VERSION main stretch" > /etc/apt/sources.list.d/koha.list
 apt install -y wget gnupg
 wget -O- http://debian.koha-community.org/koha/gpg.asc | apt-key add -
