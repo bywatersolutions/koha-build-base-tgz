2b75a2770270303cad8ceab7f5bc80119918d61f
Kyle Hall <kyle@bywatersolutions.com>
2023-02-14 14:00:25 -0500

    Fix missed stretch reference

--- pbuilder.sh
+++ pbuilder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export VERSION=22.11
-echo "deb http://debian.koha-community.org/koha $VERSION main stretch" > /etc/apt/sources.list.d/koha.list
+echo "deb http://debian.koha-community.org/koha $VERSION main buster" > /etc/apt/sources.list.d/koha.list
+apt update
 apt install -y wget gnupg
 wget -O- http://debian.koha-community.org/koha/gpg.asc | apt-key add -
 apt update
